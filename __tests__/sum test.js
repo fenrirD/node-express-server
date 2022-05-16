8c0bc2079a7fe9f70cf95bc9204eb0fb1f92f78c
fenrirD <fenrir4504@gmail.com>
2022-05-16 17:45:17 +0900

    Add test case

--- __tests__/sum test.js
+++ __tests__/sum test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 4 to equal 5', () => {
   expect(sum(1, 4)).toBe(5);
 });
+
+test('adds 1 + 67 to equal 68', () => {
+  expect(sum(1, 67)).toBe(68);
+});
